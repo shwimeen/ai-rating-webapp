705dea35eddfe2cb9e195edf388a115603724240
shwimeen <vyanabs@gmail.com>
2026-07-07 20:44:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+const tg = window.Telegram.WebApp;
+
+tg.expand();
+
+async function analyze() {
+
+    const file =
+        document.getElementById("photo").files[0];
+
+    if (!file) {
+        alert("Выберите фото");
+        return;
+    }
+
+    const formData = new FormData();
+
+    formData.append("photo", file);
+
+    formData.append(
+        "mode",
+        document.getElementById("mode").value
+    );
+
+    formData.append(
+        "age",
+        document.getElementById("age").value
+    );
+
+    formData.append(
+        "height",
+        document.getElementById("height").value
+    );
+
+    formData.append(
+        "weight",
+        document.getElementById("weight").value
+    );
+
+    document.getElementById("result").innerHTML =
+        "⏳ Анализирую...";
+
+
+    const response = await fetch(
+        "https://warrant-gravity-pro-cage.trycloudflare.com/analyze",
+        {
+            method: "POST",
+            body: formData
+        }
+    );
+
+
+    if (!response.ok) {
+        document.getElementById("result").innerHTML =
+            "❌ Ошибка сервера";
+
+        return;
+    }
+
+
+    const data = await response.json();
+
+
+    document.getElementById("result").innerHTML =
+        `
+        <h2>${data.rating}/10</h2>
+
+        <p>${data.summary}</p>
+
+        <h3>✅ Сильные стороны:</h3>
+        <ul>
+            ${data.strengths.map(item => `<li>${item}</li>`).join("")}
+        </ul>
+
+        <h3>💡 Советы:</h3>
+        <ul>
+            ${data.advice.map(item => `<li>${item}</li>`).join("")}
+        </ul>
+        `;
+}
